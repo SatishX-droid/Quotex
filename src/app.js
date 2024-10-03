cc51ac1bb5970134840c4b56f072bdd3d5687f9a
SatishX-droid <dineshdhull339@gmail.com>
2024-10-03 15:58:48 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const API_KEY = '08c4811ad76f0c4b9c2667f69f2ec8ae-a64b2339f035229d0b400001c6724487';
-const ACCOUNT_ID = '101-002-29755987-001';
+const API_KEY = '08c4811ad76f0c4b9c2667f69f2ec8ae';
+const ACCOUNT_ID = '101-002';
 const BASE_URL = 'https://api-fxpractice.oanda.com/v3/accounts/';
 const symbols = ['EUR_USD', 'GBP_USD', 'USD_JPY', 'AUD_USD']; // Add more pairs as needed
 
