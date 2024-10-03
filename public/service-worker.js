e24d13ef16eb1b0061085e73be365d86d795b878
SatishX-droid <dineshdhull339@gmail.com>
2024-10-03 15:44:50 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,21 @@
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installing.');
+    event.waitUntil(
+        caches.open('forex-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/manifest.json',
+                '/icon.png'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker activating.');
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
