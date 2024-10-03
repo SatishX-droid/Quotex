44579c6e09b7f5833dd672a4163dce6f9c35956d
SatishX-droid <dineshdhull339@gmail.com>
2024-10-03 17:20:21 +0530

    Update forexAPI.js

--- src/forexAPI.js
+++ src/forexAPI.js
@@ -1,2 +1,18 @@
-// This file can be used for more complex API interactions.
-// Currently, all API logic is in app.js.
+// src/forexAPI.js
+
+// You can export functions to fetch data from the API here
+export async function fetchForexSignals() {
+    const response = await fetch('https://api-fxpractice.oanda.com/v3/accounts/101-002-29755987-001/orders', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer 08c4811ad76f0c4b9c2667f69f2ec8ae-a64b2339f035229d0b400001c6724487',
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data.orders; // Adjust according to your API response structure
+}
