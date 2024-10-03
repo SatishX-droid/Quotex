63673868ca1cb0bf79029571a3529006b836d174
SatishX-droid <dineshdhull339@gmail.com>
2024-10-03 15:25:11 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,7 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('forex-pwa-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/src/styles.css',
-                '/src/app.js',
-                '/src/forexAPI.js'
-            ]);
-        })
-    );
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating.');
 });
